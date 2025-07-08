df3193b54ac951e8317426d936ae12491ed47688
Zulfi <zch.analytics@gmail.com>
2025-07-09 00:21:59 +0100

    Remove duplicate cypress config js files

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl and apiUrl can still be injected via GitHub Actions
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    supportFile: false,
-  }
+    baseUrl: 'https://zchresume.azureedge.net',
+    supportFile: false,  // important to avoid the support file error
+    env: {
+      apiUrl: 'https://zchresume-api.azurewebsites.net/api/visitorcounter',
+    },
+    specPattern: 'cypress/e2e/**/*.cy.js',  
+  },
 });
