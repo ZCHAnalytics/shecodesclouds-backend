0e64985ba014f344adcfb4966118cde29fd6faec
Zulfi <zch.analytics@gmail.com>
2025-07-09 00:17:09 +0100

    Edit cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl and apiUrl to be injected via GH Action
-    specPatten: 'cypress/e2e/**/*.cy.js'
+    // baseUrl and apiUrl can still be injected via GitHub Actions
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: false,
   }
 });
