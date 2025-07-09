a4e0f093fdcb4c80ed48313cfaabbd2fdfc4443a
Zulfi <zch.analytics@gmail.com>
2025-07-09 19:19:22 +0100

    Corrected letter case in function name

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     supportFile: false,  // important to avoid the support file error
     pageLoadTimeout: 100000,
     env: {
-      apiUrl: 'https://zchresume-api.azurewebsites.net/api/visitorcounter',
+      apiUrl: 'https://zchresume-api.azurewebsites.net/api/VisitorCounter',
     },
     specPattern: 'cypress/e2e/**/*.cy.js',  
   },
